fe490012f7d1c06431a9444362925bb818b6fead
Oksana <oksanabibik93@gmail.com>
2019-03-06 19:23:04 -0800

    add ex #26

---
+++ array/26_Remove_Duplicates_from_Sorted_Array/Solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    for (i = 0; i < nums.length; i++) {
+        for (j = i + 1; j < nums.length; j++) {
+            while (nums[i] == nums[j]) {
+                var removed = nums.splice(j, 1);
+            }
+        }
+    }
+    return nums;
+};
+
+console.log(removeDuplicates([1, 1])); // 1
+console.log('-----------');
+console.log(removeDuplicates([1, 1, 2])); // 2
+console.log('-----------');
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])); // 5
