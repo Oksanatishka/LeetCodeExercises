5201def010e4b30122f4c8e13866566fc202ad02
Oksana <oksanabibik93@gmail.com>
2019-03-11 13:23:35 -0700

    add ex #118

--- array/118_Pascals_Triangle/Solution.js
+++ array/118_Pascals_Triangle/Solution.js
@@ -2,6 +2,20 @@
  * @param {number} numRows
  * @return {number[][]}
  */
-var generate = function(numRows) {};
+var generate = function(numRows) {
+    var res = [];
+    for (var line = 0; line < numRows; line++) {
+        res[line] = [];
+        for (var i = 0; i <= line; i++) {
+            if (i == 0 || line == i) {
+                res[line][i] = 1;
+            } else {
+                res[line][i] = res[line - 1][i] + res[line - 1][i - 1];
+            }
+        }
+    }
+    return res;
+};
 
+console.log(generate(3));
 console.log(generate(5));
