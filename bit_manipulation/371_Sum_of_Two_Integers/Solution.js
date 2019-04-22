9a065bc406f6d0b94ecc0f77bbc37649156c53b0
Oksana <oksanabibik93@gmail.com>
2019-04-21 19:17:52 -0700

    changes ex371

--- bit_manipulation/371_Sum_of_Two_Integers/Solution.js
+++ bit_manipulation/371_Sum_of_Two_Integers/Solution.js
@@ -3,7 +3,11 @@
  * @param {number} b
  * @return {number}
  */
+
+// using a loop
 var getSum = function(a, b) {
+    if (a == 0) return b;
+    if (b == 0) return a;
     while (b != 0) {
         var carry = a & b; // carry now contains common set bits of a and b
         a = a ^ b; // Sum of bits of a and b where at least one of the bits is not set
@@ -11,6 +15,16 @@ var getSum = function(a, b) {
     }
     return a;
 };
+
+// using recursion
+// var getSum = function(a, b) {
+//     if (b == 0) {
+//         return a;
+//     }
+//     var carry = (a & b) << 1;
+//     var sum = a ^ b;
+//     return getSum(sum, carry);
+// };
 
 console.log(getSum(1, 2)); // 3     0001 + 0010 = 0011
 console.log(getSum(-2, 3)); // 1    0010 + 0011 = 0100 ??
